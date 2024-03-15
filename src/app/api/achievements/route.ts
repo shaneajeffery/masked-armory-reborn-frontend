fed528951edb3ebcf3e40e07696f1abf36007f98
Shane Jeffery <shane.jeffery54@gmail.com>
2024-03-15 13:11:37 -0400

    Get API functions firing

---
+++ src/app/api/achievements/route.ts
@@ -0,0 +1,11 @@
+export async function GET() {
+  //   const res = await fetch('https://data.mongodb-api.com/...', {
+  //     next: { revalidate: 60 }, // Revalidate every 60 seconds
+  //   });
+
+  //   const data = await res.json();
+
+  console.log(process.env);
+
+  return Response.json({});
+}
